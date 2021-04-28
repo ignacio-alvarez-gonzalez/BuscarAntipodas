dc0edc829fe81cb874291c1c48603c2376eda251
ignacio-alvarez-gonzalez <78368406+ignacio-alvarez-gonzalez@users.noreply.github.com>
2021-04-28 19:39:09 +0200

    20210428_v2

--- 20210303_EjerciciosAPI_07_Antipodas.js
+++ 20210303_EjerciciosAPI_07_Antipodas.js
@@ -9,8 +9,12 @@ require([
     "esri/Color",
     "esri/graphic",
 
+    "esri/dijit/Search",
+    "esri/tasks/locator",
+
     "dojo/ready",
     "dojo/parser",
+    "dojo/on",
 
     "dijit/layout/BorderContainer",
     "dijit/layout/ContentPane"
@@ -25,12 +29,18 @@ require([
     Color,
     Graphic,
 
+    Search,
+    Locator,
+
     ready,
     parser,
+    on
 ){
     ready(function(){
 
         parser.parse();
+
+        // SE CREAN AMBOS MAPAS:
 
         var mapa1 = new Map("divMap1", {
             basemap: "gray-vector",
@@ -44,69 +54,166 @@ require([
             zoom: 8
         });
 
-        mapa1.on("load", iniciarDibujo);
-
-        function iniciarDibujo() {
+        mapa1.on("load", function(){
             mapa1.graphics.clear();
-            dibujo = new Draw(mapa1);
+        });
+
+        // SE CREAN LAS VARIABLES NECESARIAS:
+
+        var ubicacionInicial;
+        var ubicacionAntipodas;
+        var coordenadasInicio;
+        var coordenadasAntipodas;
+
+        var simbolo = new SimpleMarkerSymbol(
+            SimpleMarkerSymbol.STYLE_CIRCLE, 
+            10,
+            new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID, new Color([0,0,0]), 1),
+            new Color([255,127,0,0.75])
+        );
+
+        var dibujo = new Draw(mapa1);
+
+        var buscadorUbicacion = new Search(
+            {map: mapa1, enableInfoWindow: false, autoNavigate: false}, 
+            "buscarUbicacion"
+        );
+        buscadorUbicacion.startup();
+
+        var geolocalizadorInicio = new Locator("http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer");
+        var geolocalizadorAntipodas = new Locator("http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer");
+
+        on(dojo.byId("seleccionarPunto"), "click", function(){
+
             dibujo.activate(Draw.POINT);
+            dibujo.on("draw-end", function(eventoSeleccion){
+
+                mapa1.graphics.remove(ubicacionInicial);
+
+                var geometria = eventoSeleccion.geometry;
+                ubicacionInicial = new Graphic(geometria, simbolo);
+                mapa1.graphics.add(ubicacionInicial);
+                mapa1.centerAndZoom(geometria, 4);
+
+                coordenadasInicio = webMercatorUtils.xyToLngLat(
+                    eventoSeleccion.geometry.x, 
+                    eventoSeleccion.geometry.y, 
+                    true
+                );
+
+                dibujo.deactivate();
+
+                geolocalizadorInicio.locationToAddress(geometria, 0);
+                geolocalizadorInicio.on("location-to-address-complete", function(resultado){
+                    dojo.byId("ubicacion").innerHTML = ubicarDireccion(resultado);
+                });
+
+            });
+
             dibujo.on("draw-end", dibujarAntipoda);
-        };
-
-        function dibujarAntipoda(evento){
-
-            console.log(evento)
-
-            // dibujo.deactivate();
-
-            var geometria = evento.geometry;
-
-            var simbolo = new SimpleMarkerSymbol(
-                SimpleMarkerSymbol.STYLE_SQUARE, 
-                10,
-                new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID, new Color([0,0,0]), 1),
-                new Color([255,127,0,0.5])
-            );
-
-            var grafico1 = new Graphic(geometria, simbolo);
-            mapa1.graphics.add(grafico1);
-
             
-            var coordenadasPunto = webMercatorUtils.xyToLngLat(evento.geometry.x, evento.geometry.y, true);
-            console.log(coordenadasPunto);
+        });
 
-            antipodaLongitud = (180-(coordenadasPunto[0]))*-1;
-            console.log(antipodaLongitud);
+        on(dojo.byId("buscarUbicacion"), "click", function(){
 
-            antipodaLatitud = (coordenadasPunto[1])*-1;
-            console.log(antipodaLatitud);
+            buscadorUbicacion.on("select-result", function(eventoBuscador){
 
-            var coordenadasAntipodas = webMercatorUtils.lngLatToXY(antipodaLongitud, antipodaLatitud);
-            console.log(coordenadasAntipodas);
+                mapa1.graphics.remove(ubicacionInicial);
+    
+                puntoInicial = new Point(eventoBuscador.result.feature.geometry);
+                ubicacionInicial = new Graphic(puntoInicial, simbolo);
+                mapa1.graphics.add(ubicacionInicial);
+                mapa1.centerAndZoom(puntoInicial, 4);
+    
+                buscadorUbicacion.clear();
+
+                coordenadasInicio = webMercatorUtils.xyToLngLat(
+                    eventoBuscador.result.feature.geometry.x, 
+                    eventoBuscador.result.feature.geometry.y, 
+                    true
+                );
+
+                dojo.byId("longitud1").innerHTML = Math.round((coordenadasInicio[0])*100)/100 + "º";
+                dojo.byId("latitud1").innerHTML = Math.round((coordenadasInicio[1])*100)/100 + "º";
+
+                geolocalizadorInicio.locationToAddress(puntoInicial, 0);
+                geolocalizadorInicio.on("location-to-address-complete", function(resultado){
+                    dojo.byId("ubicacion").innerHTML = ubicarDireccion(resultado);
+                });
+            
+            });
+
+            buscadorUbicacion.on("select-result", dibujarAntipoda);
+
+        });
+
+        function dibujarAntipoda(){
+
+            mapa2.graphics.remove(ubicacionAntipodas);
+
+            var antipodaLongitud = (180-(coordenadasInicio[0]))*-1;
+            var antipodaLatitud = (coordenadasInicio[1])*-1;
+            coordenadasAntipodas = webMercatorUtils.lngLatToXY(antipodaLongitud, antipodaLatitud);
 
             var antipoda = new Point();
             antipoda.x = coordenadasAntipodas[0];
             antipoda.y = coordenadasAntipodas[1];
-            antipoda.spatialReference = evento.geometry.spatialReference;
-            console.log(antipoda)
+            antipoda.spatialReference = mapa2.spatialReference;
 
-            var grafico2 = new Graphic(antipoda, simbolo);
-            mapa2.graphics.add(grafico2);
-
+            ubicacionAntipodas = new Graphic(antipoda, simbolo);
+            mapa2.graphics.add(ubicacionAntipodas);
             mapa2.centerAndZoom(antipoda, 4);
-            mapa2.on("zoom-end", redibujar);
+
+            dojo.byId("longitud2").innerHTML = Math.round((antipodaLongitud)*100)/100 + "º";
+            dojo.byId("latitud2").innerHTML = Math.round((antipodaLatitud)*100)/100 + "º";
+
+            geolocalizadorAntipodas.locationToAddress(antipoda, 0);
+            geolocalizadorAntipodas.on("location-to-address-complete", function(resultado){
+                dojo.byId("antipodas").innerHTML = ubicarDireccion(resultado);
+            });
 
         };
 
-        // function redibujar(){
-        //     dibujo = new Draw(mapa1);
-        //     dibujo.activate(Draw.POINT);
-        //     dibujo.on("draw-end", function(){
-        //         // mapa1.graphics.clear();
-        //         dibujarAntipoda;
-        //     });
-        // };
+        function ubicarDireccion(eventoGeolocalizador){
+            console.log(eventoGeolocalizador);
+            let direccionUbicacion = "";
+            let listado = [];
 
+            // var calle = eventoGeolocalizador.address.address.Address;
+            // if (calle != ""){listado.push(calle)};
+
+            var ciudad = eventoGeolocalizador.address.address.City;
+            if (ciudad != ""){listado.push(ciudad + ", ")};
+
+            var subregion = eventoGeolocalizador.address.address.Subregion;
+            if (subregion != ""){listado.push(subregion + ", ")};
+
+            var region = eventoGeolocalizador.address.address.Region;
+            if (region != ""){listado.push(region + ", ")};
+
+            var codigoPostal = eventoGeolocalizador.address.address.Postal;
+            if (codigoPostal != ""){listado.push(codigoPostal + ", ")};
+
+            var pais = eventoGeolocalizador.address.address.CountryCode;
+            var etiqueta = eventoGeolocalizador.address.address.LongLabel;
+
+            if (pais != ""){
+                if (eventoGeolocalizador.address.address.Type != ""){
+                    direccionUbicacion = etiqueta + ".";
+                } else {
+                    listado.push(pais + ".");
+
+                    dojo.forEach(listado, function(elemento){
+                        direccionUbicacion += elemento;
+                    });
+                };
+            } else {
+                direccionUbicacion = etiqueta + ".";
+            };
+
+            return direccionUbicacion;
+
+        };
 
     });
 
